c18e9f764a083327bb994900cb9befd4b5e1ebb5
Keith-Swamy10 <keithswamy7777@gmail.com>
2025-12-11 21:01:09 +0530

    added the azure script

---
+++ azurescript.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables (OPENAI_API_KEY)
+load_dotenv()
+
+endpoint = "https://keithopenaitokens.openai.azure.com/openai/v1"
+deployment_name = "o3-mini"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+completion = client.chat.completions.create(
+    model=deployment_name,
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the cpital of India?",
+        }
+    ],
+)
+
+print(completion.choices[0].message)
